50711059961cecc34822eb9333f6f4e54d6fece5
y <y@ylf.com>
2019-06-13 20:02:16 +0800

    2019-06-12-20:02:16-163-24

---
+++ 资料/MSP-EXP430FR6989_Software_Examples_linux/Firmware/Binary/BlinkLED_MSP430FR6989/Program BlinkLED_MSP430FR6989.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+rootDir=$(pwd)
+cd $rootDir/../MSP430Flasher
+export LD_LIBRARY_PATH=$(pwd)
+clear
+
+echo MSP-EXP430FR6989-LaunchPad Firmware Programmer
+device=MSP430FR6989
+firmware=BlinkLED_MSP430FR6989.txt
+
+echo Programming $firmware into $device ......
+./MSP430Flasher -n $device -w $rootDir/$firmware -v -g -z [VCC]
+
+read -p "Press [Enter] to continue..."
